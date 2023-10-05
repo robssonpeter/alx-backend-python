7fd775e190f92a2f0ebae8695daaae96c364d699
Peter Mgembe <peterrobsson@gmail.com>
2023-10-05 14:00:11 +0300

    fixed pycodestyle for questio zero

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
-def add(a: float, b: float)->float:
+
+def add(a: float, b: float) -> float:
     """ The function to add two numbers with explicit types """
     return a + b
