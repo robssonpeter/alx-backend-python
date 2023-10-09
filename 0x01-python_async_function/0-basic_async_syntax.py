f5ea997ff40001b92ff74aef769742b34107e50f
Ajiriwa Network <peterrobsson@gmail.com>
2023-10-09 11:46:05 +0300

    solved question zero

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+""" Async Coroutine for random delay """
+
+
+async def wait_random(max_delay: int = 10):
+    """ The function that wait for a random period of time """
+
+    number = random.randint(0, max_delay)
+    await asyncio.sleep(number)
+    return number + random.random()
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,1 @@
+The project for using Asyncio with python
