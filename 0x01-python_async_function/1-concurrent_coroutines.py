b56fa8cd1aa8fdadfe76206f7c7c14601ffd0e9b
Peter Mgembe <peterrobsson@gmail.com>
2023-10-09 14:31:24 +0300

    solved for max delay in question 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,6 +10,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List:
     """ Run n numbers of coroutines of the random function """
 
+    if (max_delay == 0):
+        return [0.0 for _ in range(n)]
     list: List = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     lst: List = await asyncio.gather(*list)
 
