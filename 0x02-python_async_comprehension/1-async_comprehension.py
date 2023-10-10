81322c3dcde05e4ba35d37660c179915fe0e4494
Peter Mgembe <peterrobsson@gmail.com>
2023-10-10 19:29:09 +0300

    added return annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """ Async generator import module """
 import asyncio
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[int, float]:
     """ The function for async comprehension """
     lst = [i async for i in async_generator()]
     return lst
