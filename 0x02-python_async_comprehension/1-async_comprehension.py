10876ce92048121b10b33f77ed316921f8e8517e
Peter Mgembe <peterrobsson@gmail.com>
2023-10-10 19:34:43 +0300

    still attempting to fix question 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,7 +6,7 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[int]:
+async def async_comprehension() -> List[float]:
     """ The function for async comprehension """
     lst = [i async for i in async_generator()]
     return lst
