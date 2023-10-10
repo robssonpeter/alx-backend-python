023720b8824843daadfd30bfa9b8ccc68a6056b4
Peter Mgembe <peterrobsson@gmail.com>
2023-10-10 09:55:29 +0300

    added annotations for question 2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,7 +3,7 @@
 from typing import List
 import asyncio
 
-wait_random: function = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n, max_delay) -> float:
+def measure_time(n:int, max_delay: int) -> float:
     """ The function to keep track of the execution time """
     asyncio.run(wait_n(n, max_delay))
     time_taken = time.perf_counter()
