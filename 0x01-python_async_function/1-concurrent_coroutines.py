708cc4c229715abb109be037ddbea408c3201c84
Peter Mgembe <peterrobsson@gmail.com>
2023-10-13 12:39:04 +0300

    solving question 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float, None]:
     """ Run n numbers of coroutines of the random function """
 
     if (max_delay == 0):
