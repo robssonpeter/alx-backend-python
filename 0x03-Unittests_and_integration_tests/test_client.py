9a9270e3641e16c96792435259ba48b8d8663d8b
Peter Mgembe <peterrobsson@gmail.com>
2023-10-31 05:39:19 +0300

    tried question 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,10 +1,21 @@
 import unittest
+from unittest.mock import patch
 from client import GithubOrgClient
+from client import get_json
+from parameterized import parameterized
 """ The module for testing client.py"""
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    def test_org(self):
-        client = GithubOrgClient("parker")
-        output = 'https://api.github.com/orgs/parker'
+    @parameterized.expand([
+        ('google', ),
+        ('abc')
+    ])
+    @patch('client.get_json')
+    def test_org(self, input, mocked):
+        mocked.return_value = 'done'
+        client = GithubOrgClient(input)
+        
+        output = f'https://api.github.com/orgs/{input}'
         self.assertEqual(client.ORG_URL, output)
+        
