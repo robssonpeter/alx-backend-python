8ede97f3e6d7d396a46322ab47607c31cc9b9e6c
Peter Mgembe <peterrobsson@gmail.com>
2023-11-03 17:48:06 +0300

    tried to fix question 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,26 +1,29 @@
+#!/usr/bin/env python3
+""" The module for testing client.py"""
 import unittest
 from unittest.mock import patch
 from client import GithubOrgClient
 from client import get_json
 from parameterized import parameterized
-""" The module for testing client.py"""
 
 
 class TestGithubOrgClient(unittest.TestCase):
+    """ The Class for testing github organizaiton api request """
     @parameterized.expand([
         ('google', ),
         ('abc')
     ])
     @patch('client.get_json')
     def test_org(self, input, mocked):
+        """ Test to see if it returs the correct organization """
         mocked.return_value = 'done'
         client = GithubOrgClient(input)
-        
+
         output = f'https://api.github.com/orgs/{input}'
         self.assertEqual(client.ORG_URL, output)
-    
-    @parameterized.expand([
+
+    """@parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license"),
         ({"license": {"key": "other_license"}}, "my_license"),
     ])
-    def test_has_license(self):
+    def test_has_license(self):"""
